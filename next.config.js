5f7b0155db137b6702bd37fc2058f875d67b9b91
Alex | Oruga De la Mora <87877932+Oruga420@users.noreply.github.com>
2025-11-12 17:49:47 -0500

    Use Next Image for header logos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.assent.com',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
